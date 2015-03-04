0d14502f8220a478c26c5592a5551b0fc9f97e63
Kin Lane <kinlane@gmail.com>
2015-03-03 18:54:04 -0800

    debug

--- js/editor-question-json.js
+++ js/editor-question-json.js
@@ -156,7 +156,7 @@ function getQuestion($question_question,$question_answer,$question_host,$questio
     html = html + '<table border="0" width="95%">';
     
     html = html + '<tr>';
-    html = html + '<td align="right" style="" width="40%"><strong>' + $question_question + ':</strong> (' + $question_host + $question_baseurl + $question_path +  ' ' + $question_method + ')</td>';
+    html = html + '<td align="right" style="" width="70%"><strong>' + $question_question + ':</strong> (' + $question_host + $question_baseurl + $question_path +  ' ' + $question_method + ')</td>';
     html = html + '<td align="left" style="" width="40%">';
     
     html = html + $question_answer;
