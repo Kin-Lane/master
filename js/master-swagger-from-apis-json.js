7a80ff8c771245198824ffeb5154c6a18d679bde
Kin Lane <info@apievangelist.com>
2015-04-12 13:55:25 -0700

    rollback

--- js/master-swagger-from-apis-json.js
+++ js/master-swagger-from-apis-json.js
@@ -12,16 +12,15 @@ $apipropertyoptions = "";
 	
   function deploySwaggerMaster($swaggerURL)
   	{		
-  	
-  		console.log('in');
-  	
+  		
   	  $swaggerContainer = "swagger-ui-container-" + $includecount;	
   		
-	  $html = '<div id="' + $swaggerContainer + '" class="swagger-ui-wrap"></div>';
+	  $html = '<div class="swagger-section">';
+	  $html = $html + '<div id="message-bar" class="swagger-ui-wrap">&nbsp;</div>';
+	  $html = $html + '<div id="' + $swaggerContainer + '" class="swagger-ui-wrap"></div>';
+	  $html = $html + '</div>';
 	  
-	  console.log($html);
-	  
-	  $('#swagger-section').append($html);  
+	  $('#master-swagger-section').append($html);  
   		
 	  //var url = "https://kin-lane.github.io/" + $repo + "/swagger.json";
 	  
