2860256cfaa7d907283f412c300e404542b69cc5
Kin Lane <info@apievangelist.com>
2015-04-12 14:49:50 -0700

    rollback

--- js/master-swagger-from-apis-json.js
+++ js/master-swagger-from-apis-json.js
@@ -9,33 +9,113 @@ $includecount = 1;
 $MasterAPISJSON = "";
 
 $apipropertyoptions = "";
+	
+  function deploySwaggerMaster($swaggerURL,$itemcount)
+  	{		
+  		
+  	  $swaggerContainer = "swagger-ui-container-" + $itemcount;	
+  		
+	  $html = '<div class="swagger-section">';
+	  $html = $html + '<div id="message-bar" class="swagger-ui-wrap">&nbsp;</div>';
+	  $html = $html + '<div id="' + $swaggerContainer + '" class="swagger-ui-wrap"></div>';
+	  $html = $html + '</div>';
+	  console.log($html);
+	  $('#master-swagger-section').append($html);  
+  		
+	  //var url = "https://kin-lane.github.io/" + $repo + "/swagger.json";
+	  
+	  window.swaggerUi = new SwaggerUi({
+	  	
+	    url: $swaggerURL,
+	    dom_id: $swaggerContainer,
+	    validatorUrl: null,
+	    supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
+	    onComplete: function(swaggerApi, swaggerUi){
+	
+	      var textboxes = document.getElementsByTagName("input");        
 
-function waitforit($showme)
-	{
-	console.log("waiting..." + $showme);
-	}
+		  if($apikeys["API Evangelist"] === false)
+		  	{
+	      $appid = $apikeys["API Evangelist"]['appid'];
+	      $appkey = $apikeys["API Evangelist"]['appkey'];
 
-function loadMasterSwaggerFromAPIsJSON($url)
+			for (var i=0;i<textboxes.length;i++)
+			 	{
+			    var textbox = textboxes[i];
+			    if (textbox.type.toLowerCase() == "text")
+			       {
+			       if(textbox.name=='appid')
+			       	{
+			       	textboxes[i].value = $appid	;
+			       	}
+			       if(textbox.name=='appkey')
+			       	{
+			       	textboxes[i].value = $appkey;	
+			       	}			       	
+			     }
+			 } 
+			 }	
+	
+	      $('pre code').each(function(i, e) {
+	        hljs.highlightBlock(e)
+	      });
+	      
+	    },
+	    onFailure: function(data) {
+	      log("Unable to Load SwaggerUI");
+	    },
+	    docExpansion: "none",
+	    sorter : "alpha"
+	  });
+	
+	  window.swaggerUi.load();	
+  }
+
+function loadMasterSwaggerFromAPIsJSON($apisjsonURL)
     {
 
-	console.log("load..." + $url);
+	console.log("loading..." + $apisjsonURL);
 
-	$.getJSON($url, function(apisJSON) { 													
+	var jqxhr = $.getJSON($apisjsonURL, function(apisJSON) { 													
 
 
 		buildMasterSwaggerFromAPIsJSON(apisJSON);
 
-	});	  
+	});	
+
+	// Set another completion function for the request above
+	jqxhr.complete(function() {
+		
+	  	//document.getElementById("jsonNavigator").style.display=''; 
+	  	                 
+        });		  
          	  	
-    }   
+    } 
+    
+function loadSwaggerFromAPIsJSON($apisjsonURL,$itemcount)
+    {
+
+	console.log("processing..." + $apisjsonURL);
+
+	var jqxhr = $.getJSON($apisjsonURL, function(apisJSON) { 													
+
+
+		buildSwaggerFromAPIsJSON(apisJSON,$itemcount);
+
+	});	
+
+	// Set another completion function for the request above
+	jqxhr.complete(function() {
+		
+	  	//document.getElementById("jsonNavigator").style.display=''; 
+	  	                 
+        });		  
+         	  	
+    }     
 
 function buildMasterSwaggerFromAPIsJSON(apisJSON)
 	{
 	//console.log("run2");	
-	
-	mySwagger = Array();
-	
-	$itemcount = 1;
 		
 	$apisJSONName = apisJSON['name'];
 
@@ -57,98 +137,58 @@ function buildMasterSwaggerFromAPIsJSON(apisJSON)
      	 $includeRootUrl = apiVal['url'];	      	 
      	// $includeUrl = $includeRootUrl.replace("apis.json","");	 
 		 //console.log($includecount);
-		 if($includecount< 6)
+		 if($includecount< 5)
 		 	{
-			
-			$.getJSON($includeRootUrl, function(apisJSON) { 													
-		
-			$apisJSONName = apisJSON['name'];
-		
-		 	$apisJSONDesc = apisJSON['description'];
-		 	$apisJSONLogo = apisJSON['image'];
-		 	$apisJSONURL = apisJSON['url'];
-		 	  
-		    apisJSONTags = apisJSON['tags'];            
-		    apisJSONAPIs = apisJSON['apis'];
-		    
-		     $.each(apisJSONAPIs, function(apiKey, apiVal) { 
-		
-		     	 $apiName = apiVal['name']; 
-		     	 $apiDesc = apiVal['description'];
-		     	 $apiImage = apiVal['image']; 
-		     	 $apiHumanURL = apiVal['humanURL']; 
-		     	 $apiBaseURL = apiVal['baseURL'];               	                         	 
-				 $apiTags = apiVal['tags'];			 	 
-				 
-				 $apiProperties = apiVal['properties'];
-				 $.each($apiProperties, function(propertyKey, propertyVal) { 
-		
-				 	$propertyType = propertyVal['type'];
-				 	$propertyURL = propertyVal['url'];					 				 			 							 		 					 	
-				 	//console.log($propertyType);
-				 	
-				    if($propertyType=='swagger'||$propertyType=='Swagger')
-				    	{
-				    		
-			
-				    	$swaggerURL = $propertyURL;
-				    	console.log("Loading Swagger: " + $propertyURL);	
-				    	
-				    	  swaggerUi = "";
-				    	
-					  	  $swaggerContainer = "swagger-ui-container-" + $itemcount;	
-					  		
-						  $html = '<div class="swagger-section">';
-						  $html = $html + '<div id="message-bar" class="swagger-ui-wrap">&nbsp;</div>';
-						  $html = $html + '<div id="' + $swaggerContainer + '" class="swagger-ui-wrap"></div>';
-						  $html = $html + '</div>';
-						  console.log("container: " + $swaggerContainer);
-						  
-						  $('#master-swagger-section').append($html);  
-					  		
-						  //var url = "https://kin-lane.github.io/" + $repo + "/swagger.json";
-						  
-						  mySwagger[$itemcount] = new SwaggerUi({
-						  	
-						    url: $swaggerURL,
-						    dom_id: $swaggerContainer,
-						    validatorUrl: null,
-						    supportedSubmitMethods: ['get', 'post', 'put', 'delete', 'patch'],
-						    onComplete: function(swaggerApi, swaggerUi){
-						
-						      $('pre code').each(function(i, e) {
-						        hljs.highlightBlock(e)
-						      });
-						      
-						    },
-						    onFailure: function(data) {
-						      log("Unable to Load SwaggerUI");
-						    },
-						    docExpansion: "none",
-						    sorter : "alpha"
-						  });
-						
-		
-						  setTimeout(mySwagger[$itemcount].load(), 25000);
-						  	    	
-				    	  $itemcount++;
-				    	  
-				    	}	 	
-			
-				 	$propertycount++;
-				 	
-				 	}); 				 	                                           
-		        				 					 				 	 				 					 											
-				 $apiContact = apiVal['contact'];
-				 $apicount++;										
-			});
-		
-			});	
-
+		 	setTimeout(loadSwaggerFromAPIsJSON($includeRootUrl,$includecount), 3000);
+		 	//loadSwaggerFromAPIsJSON($includeRootUrl,$includecount);
+		 	//console.log("include (" + $includecount + "):" + $includeRootUrl);
 		 	}		 		 
  
 		 $includecount++;										
 	});	
 
 	}
+	
+function buildSwaggerFromAPIsJSON(apisJSON,$itemcount)
+	{
+	//console.log("run3");	
+		
+	$apisJSONName = apisJSON['name'];
 
+ 	$apisJSONDesc = apisJSON['description'];
+ 	$apisJSONLogo = apisJSON['image'];
+ 	$apisJSONURL = apisJSON['url'];
+ 	  
+    apisJSONTags = apisJSON['tags'];            
+    apisJSONAPIs = apisJSON['apis'];
+    
+     $.each(apisJSONAPIs, function(apiKey, apiVal) { 
+
+     	 $apiName = apiVal['name']; 
+     	 $apiDesc = apiVal['description'];
+     	 $apiImage = apiVal['image']; 
+     	 $apiHumanURL = apiVal['humanURL']; 
+     	 $apiBaseURL = apiVal['baseURL'];               	                         	 
+		 $apiTags = apiVal['tags'];			 	 
+		 
+		 $apiProperties = apiVal['properties'];
+		 $.each($apiProperties, function(propertyKey, propertyVal) { 
+
+		 	$propertyType = propertyVal['type'];
+		 	$propertyURL = propertyVal['url'];					 				 			 							 		 					 	
+		 	//console.log($propertyType);
+		    if($propertyType=='swagger'||$propertyType=='Swagger')
+		    	{
+		    	console.log("Loading Swagger: " + $propertyURL);	
+		    	deploySwaggerMaster($propertyURL,$itemcount)
+		    	}	 	
+	
+		 	$propertycount++;
+		 	
+		 	}); 				 	                                           
+        				 					 				 	 				 					 											
+		 $apiContact = apiVal['contact'];
+		 $apicount++;										
+	});
+		
+	}	
