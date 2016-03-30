003fb45869cb0846a1824d367a350b57593fee31
Kin Lane <info@apievangelist.com>
2016-03-29 23:10:47 -0700

    debug

--- js/apis-json-include-list.js
+++ js/apis-json-include-list.js
@@ -5,11 +5,11 @@ function APIJSONGetIncludeListing($includeName,$includeRootUrl,$includeUrl,$incl
 
 	if($includeToggle == 0 )
 		{
-  	html = '<div style="float:right; width: 325px; border: 1px solid #000; padding-right: 35px;">';
+  	html = '<div style="float:right; width: 325px; border: 0px solid #000; padding-right: 35px;">';
 		}
 	else
 		{
-		html = '<div style="float:left; width: 325px; border: 1px solid #000; padding-left: 35px;">';
+		html = '<div style="float:left; width: 425px; border: 0px solid #000; padding-left: 35px;">';
 		}
   html = html + '<a href="' + $includeUrl + '" style="color: #000; font-size: 18px; text-decoration: none;" title="' + $includeName + '"><strong>' + $includeName + '</strong></a>';
   html = html + '</div>';
